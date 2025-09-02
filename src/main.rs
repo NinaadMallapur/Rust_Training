ab1e1105fe7efcf4a3ca0706bdd779ec48f3d600
Ninaad Mallapur <ninaadmallapur@Ninaads-MacBook-Air.local>
2025-09-01 23:49:17 -0700

    feat: write push function - initial and basic implementation

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,32 @@ impl CircularBuffer {
             tail: -1,
         }
     }
+
+    fn push(&mut self, value: i32) -> Result<(), Errors> {
+        self.head = self.head + 1;
+        self.array[self.head as usize] = value;
+
+        Ok(())
+    }
 }
 
-fn main() {
+fn main() {}
+
+mod tests {
+    use crate::CircularBuffer;
+    use crate::Errors;
+
+    #[test]
+    fn test_push() {
     let mut cb = CircularBuffer::new();
+
+        let _ = cb.push(9);
+        assert_eq!(cb.array[0], 9);
+
+        let _ = cb.push(99);
+        assert_eq!(cb.array[1], 99);
+
+        let _ = cb.push(102);
+        assert_eq!(cb.array[2], 102);
+    }
 }
